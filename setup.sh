bfea362bfb03e3601bf8445afd0de72034ccdbeb
thxxx <zxcv05999@naver.com>
2025-08-15 17:51:44 +0900

    whisper inference

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 apt-get update
 apt-get install -y ffmpeg
 python3 -m pip install flask flask-cors openai elevenlabs fastapi uvicorn[standard] orjson
+python3 -m pip install faster-whisper soundfile librosa
 
 # uvicorn server:app --host 0.0.0.0 --port 5000
